d3e97d50067adc82c679587626523db04b1e7e9e
Kirill Khudyakov <hudiakov@gmail.com>
2020-05-27 23:34:15 +0300

    Добавил модели

---
+++ CinemaApp/CinemaApp/Models/Movie.swift
@@ -0,0 +1,28 @@
+//
+//  Movie.swift
+//  CinemaApp
+//
+//  Created by Kirill Khudiakov on 27.05.2020.
+//  Copyright © 2020 Kirill Khudiakov. All rights reserved.
+//
+
+import Foundation
+
+
+struct Movie {
+    let screenshots: [String]
+    let title: String
+    let janrs: [String]
+    let type: String
+    let note: String
+}
+
+
+struct Ticket {
+    let date: String
+    let time: String
+    let seats: String
+    let title: String
+    let total: String
+}
+
